6f15c97e8703de150b660a66bcfabb696609151f
Pineapple <Pineapple@Users-MacBook-Pro.local>
2017-08-06 22:30:47 -0700

    initial commit

--- app/app.js
+++ app/app.js
@@ -1,22 +1,26 @@
-import 'normalize.css/normalize.css';
-import 'assets/stylesheets/main.css';
+// import 'normalize.css/normalize.css';
+// import 'assets/stylesheets/main.css';
 import { createStore } from 'redux';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1;
-  }
+const reducer = (state, action) => state; 
+const store = createStore(reducer); 
 
-  return state;
-};
 
-const initialState = 0;
 
-const store = createStore(reducer, initialState);
 
-store.subscribe(() => document.getElementById('counter').innerText = store.getState());
 
-setInterval(() => store.dispatch({ type: 'INC' }), 500);
 
-console.log("Redux started");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
